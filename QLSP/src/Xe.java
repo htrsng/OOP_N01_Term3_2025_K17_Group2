638225ac33215979559b5d0ff2ff546b45ea8f42
htrsng <tranghuyen20051312@gmail.com>
2025-05-10 20:14:41 +0700

    capnhat

--- QLSP/src/Xe.java
+++ QLSP/src/Xe.java
@@ -1,3 +1,43 @@
+package QLSP;
+
 public class Xe {
-    
+    private String maXe;
+    private String tenXe;
+    private String hangXe;
+    private double gia;
+    private int soLuong;
+    private String trangThai;
+
+    public Xe(String maXe, String tenXe, String hangXe, double gia, int soLuong, String trangThai) {
+        this.maXe = maXe;
+        this.tenXe = tenXe;
+        this.hangXe = hangXe;
+        this.gia = gia;
+        this.soLuong = soLuong;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaXe() {
+        return maXe;
+    }
+
+    public String getTenXe() {
+        return tenXe;
+    }
+
+    public String getHangXe() {
+        return hangXe;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public String getTrangThai() {
+        return trangThai;
+    }
 }
--- QLSP/test/XeTest.java
+++ QLSP/test/XeTest.java
@@ -1,5 +1,17 @@
 package QLSP.test;
 
+import QLSP.Xe;
+import java.io.PrintStream;
+
 public class XeTest {
-    
+    public static void main(String[] args) throws Exception {
+        Xe xe = new Xe("01", "Honda SH", "Honda", 80000000, 5, "Còn hàng");
+
+        System.out.println("Ma: " + xe.getMaXe());
+        System.out.println("Hang: " + xe.getHangXe());
+        System.out.println("Ten: " + xe.getTenXe());
+        System.out.println("Gia: " + xe.getGia());
+        System.out.println("Sl: " + xe.getSoLuong());
+        System.out.println("Tt: " + xe.getTrangThai());
+    }
 }
