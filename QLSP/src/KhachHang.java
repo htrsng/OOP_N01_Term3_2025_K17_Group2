37b004050036aee833fad38ac0164224e997c351
DangDacTu <dagdtus@gmail.com>
2025-05-11 16:20:23 +0700

    .

--- QLSP/src/KhachHang.java
+++ QLSP/src/KhachHang.java
@@ -1,3 +1,34 @@
+package QLSP;
 
+public class KhachHang {
+    private String maDon;
+    private String tenKhach;
+    private long sdt;
+    private String maXe;
+
+    public KhachHang(String maDon, String tenKhach, long sdt, String maXe){
+        this.maDon = maDon;
+        this.tenKhach = tenKhach;
+        this.sdt = sdt;
+        this.maXe = maXe;
+    }
+
+    public String getMaDon(){
+        return maDon;
+    }
+
+    public String getTenKhach(){
+        return tenKhach;
+    }
+
+    public long getSDT(){
+        return sdt;
+    }
+
+    public String getMaXe(){
+        return maXe;
+    }
+    
+}
     
 
--- QLSP/test/KhachHangTest.java
+++ QLSP/test/KhachHangTest.java
@@ -1,6 +1,21 @@
 package QLSP.test;
 
-import java.util.Scanner;
+import QLSP.KhachHang;
+import java.io.PrintStream;
+
+public class KhachHangTest {
+
+    public static void main(String[] args) {
+        QLSP.src.KhachHang khachhang = new KhachHang("001", "Dang Dac Tu", 2243456789, 05);
+
+        System.out.println("Ma:"+khachhang.getMaDon());
+        System.out.println("Ten:"+khachhang.getTenKhach());
+        System.out.println("SDT:"+khachhang.getSDT());
+        System.out.println("Ma xe:" + khachhang.getMaXe());
+    }
+}
+
+/*import java.util.Scanner;
 
 public class KhachHangTest {
     public static void main(String[] args) {
@@ -19,6 +34,7 @@ public class KhachHangTest {
         /*System.out.println("list customer");
         for(int i = 0; i < amount; i++){
             System.out.println(name[i]);
-        }*/
-    }
+        }*
+    }*
 }
+*/
