3c01f6ad2b9606fe0a0fecebe60a59edb56b1dd4
htrsng <tranghuyen20051312@gmail.com>
2025-05-14 16:41:23 +0700

    chinh sua

--- QLSP/test/KhachHangTest.java
+++ QLSP/test/KhachHangTest.java
@@ -15,26 +15,3 @@ public class KhachHangTest {
     }
 }
 
-/*import java.util.Scanner;
-
-public class KhachHangTest {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("list name customer for testing");
-        System.out.println("amount of customer");
-        int amount = scan.nextInt();
-        
-        String[] name = new String[amount];
-
-        System.out.println("input name:");
-        for(int i =0; i < amount; i++){
-            name[i] = scan.next();
-        }
-        // output
-        /*System.out.println("list customer");
-        for(int i = 0; i < amount; i++){
-            System.out.println(name[i]);
-        }*
-    }*
-}
-*/
