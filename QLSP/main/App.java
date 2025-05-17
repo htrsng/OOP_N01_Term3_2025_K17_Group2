3c43bb7c9bed37889a205cba6920ee1eb379539a
dungluuquang <151888457+dungluuquang@users.noreply.github.com>
2025-05-17 21:38:54 +0700

    Update App.java

--- QLSP/main/App.java
+++ QLSP/main/App.java
@@ -1,19 +1,9 @@
-//import java.time.LocalDate;
-package QLSP.main;
-
-import QLSP.test.DonDatCocTest;
-import QLSP.test.KhachHangTest;
-import QLSP.test.XeTest;
-
+import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
-        DonDatCocTest.TestDatCoc();
-        KhachHangTest.TestKhanhHang();
-        
-        // Tạo một đối tượng DonDatCoc
         // Tạo một đối tượng Xe
-       /*  System.out.println("\n Thong tin xe trong cua hang\n");
+        System.out.println("\n Thong tin xe trong cua hang\n");
         Xe xe = new Xe("01", "Honda SH", "Honda", 80000000, 5, "Con");
 
         System.out.println("Ma xe: " + xe.getMaXe());
@@ -24,7 +14,7 @@ public class App {
         System.out.println("Trangthai: " + xe.getTrangThai());
         
         // Tạo một đối tượng khach hang
-         KhachHang kh = new KhachHang(
+        KhachHang kh = new KhachHang(
             "KH01",                 
             "Nguyen Van A",          
             "0123456789",           
@@ -39,6 +29,18 @@ public class App {
         System.out.println("Sdt: " + kh.getSoDienThoai());
         System.out.println("Maxe: " + kh.getMaXe());
         System.out.println("Ngayban: " + kh.getNgayBan());
-        */
+        
+        // Tạo một đối tượng DonDatCoc
+        DonDatCoc donDatCoc = new DonDatCoc("DDC01", "01", "Honda SH", "Honda", 8000000, 1, "Con");
+        
+        // In thông tin đơn đặt cọc
+        System.out.println("\n Thong tin Don Dat Coc\n");
+        System.out.println("Ma don: " + donDatCoc.getMaDon());
+        System.out.println("Ma xe: " + donDatCoc.getMaXe());
+        System.out.println("Ten xe: " + donDatCoc.getTenXe());
+        System.out.println("Hang xe: " + donDatCoc.getHangXe());
+        System.out.println("Gia: " + donDatCoc.getGia());
+        System.out.println("Soluong: " + donDatCoc.getSoLuong());
+        System.out.println("Trangthai: " + donDatCoc.getTrangThai());
     }
 }
